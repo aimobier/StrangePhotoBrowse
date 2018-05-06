501aebb2d151f23ac8235847a60ed3bfc1177424
荆文征 <200739491@qq.com>
2018-05-06 11:28:16 +0800

    增加 爬虫 爬取美女图片 进行测试

---
+++ Spider/index.js
@@ -0,0 +1,26 @@
+const request = require('superagent');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const url = require('url');
+
+for (var i = 0; i < 18; i++) {
+
+  request
+    .get('https://movie.douban.com/celebrity/1322189/photos/?=0')
+    .query({ start: i*30 }) // query string
+    .end((err, res) => {
+      // Do something
+      const $ = cheerio.load(res.text);
+      $('div.article li img').each(function(i, elem) {
+
+        const imgSrc = $(this).attr("src");
+
+        const loc = url.parse(imgSrc).pathname.split("/")[5];
+
+        var stream = fs.createWriteStream('download/'+loc);
+        var req = request.get(imgSrc);
+        req.pipe(stream);
+
+      });
+    });
+}
